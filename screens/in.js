c1b27a98f502c5d7bab60ae20e2eb1d3e2a7140f
AarthSingal <70443223+AarthSingal@users.noreply.github.com>
2021-05-04 14:06:11 +0530

    Add files via upload

---
+++ screens/in.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Instagram extends React.Component {
+  render (){
+  return (
+    <View >
+      <Text>Instagram</Text>
+    </View>
+  );}
+}
